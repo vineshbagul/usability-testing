89046674cc8c3fd1e67ccd2818d4e3ac1aa16dc0
vinesh <vineshbagul93gmail.com>
2022-02-27 14:15:15 +0100

    push method created

--- assignment1.js
+++ assignment1.js
@@ -11,5 +11,24 @@ class Stack
         this.capacity=capacity;
     }
 
+    /***********************************
+******pushing into stack **********
+***********************************/
+push(value)
+{
+    if(this.head==this.capacity-1)
+    {
+        console.log("Overflow");
+    }
+    else
+    {
+        this.head +=1;
+        this.array[this.head]=value;
+        console.log(`${this.array[this.head]} is inserted`);
+        return this.head;
+    }
+
+}
+
 
 }
